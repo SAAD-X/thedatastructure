f1f70a6b27f1f12a1b5802af5d6a4863bdb34191
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2021-02-16 20:52:42 +0600

    Add files via upload

---
+++ Lab/Star.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char arr[5] = {'*','*','*','*','*'};
+
+    for (int i = 0; i <5; i++)
+    {
+        cout<<arr[i];
+    }
+    return 0;
+}
